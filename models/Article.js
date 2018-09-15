e1391eb42778749d78c9668464e769ba848b6f0e
Ananya Bala <ananyabala42@gmail.com>
2018-09-14 22:06:06 -0400

    just showing undefined on the website route

--- models/Article.js
+++ models/Article.js
@@ -2,17 +2,13 @@ var mongoose = require("mongoose");
 
 // Save a reference to the Schema constructor
 var Schema = mongoose.Schema;
+console.log("This is the Schema" + Schema);
 
 // Using the Schema constructor, create a new UserSchema object
 // This is similar to a Sequelize model
 var ArticleSchema = new Schema({
   // `title` is required and of type String
   headline: {
-    type: String,
-    required: true,
-    unique: { index: { unique: true } }
-  },
-  summary: {
     type: String,
     required: true
   },
@@ -21,25 +17,22 @@ var ArticleSchema = new Schema({
     type: String,
     required: true
   },
+  summary: {
+    type: String,
+    
+  },
   // `note` is an object that stores a Note id
   // The ref property links the ObjectId to the Note model
   // This allows us to populate the Article with an associated Note
-  // comment: {
-  //   type: Schema.Types.ObjectId,
-  //   ref: "Comment"
-  // }
-  date: {
-    type: Date,
-    default: Date.now
-  },
-  saved: {
-    type: Boolean,
-    default: false
+  note: {
+    type: Schema.Types.ObjectId,
+    ref: "Note"
   }
 });
 
 // This creates our model from the above schema, using mongoose's model method
 var Article = mongoose.model("Article", ArticleSchema);
+console.log("This is 12345" + Article);
 
 // Export the Article model
 module.exports = Article;
